d0ee98a491875ebcefe7671e95866e21e508b9a8
Idiyatov <ildarthegreat@gmail.com>
2021-09-21 22:17:06 +0400

    ЛАБА 2. ЗАДАНИЕ 1 Добавить в интерфейс MathFunction метод: CompositeFunction andThen(MathFunction afterFunction) – с реализацией по умолчанию...

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -2,4 +2,8 @@ package functions;
 
 public interface MathFunction {
     double apply(double x);
+    default CompositeFunction andThen(MathFunction afterFunction){
+        return new CompositeFunction(this,afterFunction);
+    }
+
 }
