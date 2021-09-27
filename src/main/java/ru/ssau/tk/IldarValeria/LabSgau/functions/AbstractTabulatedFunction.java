523dfe5a238543e6ba67540aab38a691ce09e33c
Idiyatov <ildarthegreat@gmail.com>
2021-09-27 18:37:35 +0400

    ЛАБА 2. ЗАДАНИЕ 2 Исправлено: Добавлен абстракный класс  AbstractTabulatedFunction , с соответсвующими абстрактными и реализованными методами.

---
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.IldarValeria.LabSgau.functions;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction {
+    public int count;
+
+    protected abstract int floorIndexOfX(double x);
+
+    protected abstract double extrapolateLeft(double x);
+
+    protected abstract double extrapolateRight(double x);
+
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + (rightY - leftY) * (x - leftX) / (rightX - leftX);
+    }
+
+    public double apply(double x) {
+        if (x < leftBound()) return extrapolateLeft(x);
+        if (x > rightBound()) return extrapolateRight(x);
+        if (indexOfX(x) != -1) return getY(indexOfX(x));
+        return interpolate(x, floorIndexOfX(x));
+    }
+
+
+}
