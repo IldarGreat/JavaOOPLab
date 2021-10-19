95522b34f25286f8b46bc99eecbffdfff6424483
Idiyatov <ildarthegreat@gmail.com>
2021-10-19 18:14:20 +0400

    ЛАБА 3.ЗАДАНИЕ 1 Создан класс Point, содержащий только два публичных завершённых поля типа double: x и y.

---
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.IldarValeria.LabSgau.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
