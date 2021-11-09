dbc35cf50f2829fefd3f05230c474596a8beb13c
Idiyatov <ildarthegreat@gmail.com>
2021-11-09 11:24:26 +0400

    ЛАБА 3.ЗАДАНИЕ 3(Y) В классе FunctionsIO добавлен статический метод: void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function).Создан в пакете io класс TabulatedFunctionFileWriter с main-методом.После запуска main метода  файлы появились в директории output и их содержание корректно.

--- src/main/java/ru/ssau/tk/IldarValeria/LabSgau/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/io/FunctionsIO.java
@@ -1,8 +1,22 @@
 package ru.ssau.tk.IldarValeria.LabSgau.io;
 
+import ru.ssau.tk.IldarValeria.LabSgau.functions.*;
+
+import java.io.*;
+
 public final class FunctionsIO {
 
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
+    }
+
 }
