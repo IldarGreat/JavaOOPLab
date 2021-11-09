56de34f3388fe17a21e6b2e782e3006ce87977b1
Idiyatov <ildarthegreat@gmail.com>
2021-11-09 11:57:00 +0400

    ЛАБА 3.ЗАДАНИЕ 3(X) В классе FunctionsIO добавлен статический метод: void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function).Создан в пакете io класс TabulatedFunctionFileOutputStream с main методом.Запущен метод,файлы появились в директории output

--- src/main/java/ru/ssau/tk/IldarValeria/LabSgau/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/io/FunctionsIO.java
@@ -19,4 +19,14 @@ public final class FunctionsIO {
         printWriter.flush();
     }
 
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
+    }
+
 }
