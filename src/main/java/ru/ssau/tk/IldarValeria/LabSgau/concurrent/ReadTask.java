f99861136b0af2c890c3c74a244a31a6dbcd48c5
Idiyatov <ildarthegreat@gmail.com>
2021-11-13 21:39:33 +0400

    ЛАБА 3.ЗАДАНИЕ 4(X) В пакете concurrent создать класс ReadTask, реализующий интерфейс Runnable.

---
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/concurrent/ReadTask.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.IldarValeria.LabSgau.concurrent;
+
+import ru.ssau.tk.IldarValeria.LabSgau.functions.*;
+
+public class ReadTask implements Runnable {
+    private final TabulatedFunction tabulatedFunction;
+
+    public ReadTask(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for (int element = 0; element < tabulatedFunction.getCount(); element++) {
+            System.out.printf("After read: i = %d, x = %f, y = %f", element, tabulatedFunction.getX(element), tabulatedFunction.getY(element));
+        }
+    }
+}
