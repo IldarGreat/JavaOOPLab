44dd1dfae28ffec5384bfe97284003183852a096
Idiyatov <ildarthegreat@gmail.com>
2021-11-14 12:22:57 +0400

    ЛАБА 4.ЗАДАНИЕ 1(X) В пакете concurrent создан класс WriteTask, реализующий интерфейс Runnable.

---
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/concurrent/WriteTask.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.IldarValeria.LabSgau.concurrent;
+
+import ru.ssau.tk.IldarValeria.LabSgau.functions.*;
+
+public class WriteTask implements Runnable {
+    private final TabulatedFunction tabulatedFunction;
+    private final double value;
+
+    public WriteTask(TabulatedFunction tabulatedFunction, double value) {
+        this.tabulatedFunction = tabulatedFunction;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < tabulatedFunction.getCount(); index++) {
+            tabulatedFunction.setY(index, value);
+            System.out.printf("Writing for index %d complete", index);
+        }
+    }
+}
