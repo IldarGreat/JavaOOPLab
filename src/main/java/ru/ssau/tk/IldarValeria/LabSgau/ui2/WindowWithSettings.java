d5175883f7c9bb2d38b0ca2a9ed6436261e62006
Ildar <ildarthegreat@gmail.com>
2021-12-29 14:31:45 +0400

    ui2

--- src/main/java/ru/ssau/tk/IldarValeria/LabSgau/ui2/WindowWithSettings.java
+++ src/main/java/ru/ssau/tk/IldarValeria/LabSgau/ui2/WindowWithSettings.java
@@ -7,7 +7,6 @@ import ru.ssau.tk.IldarValeria.LabSgau.functions.factory.TabulatedFunctionFactor
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ItemEvent;
-import java.util.ArrayList;
 
 public class WindowWithSettings extends JDialog {
     JTabbedPane tabbedPane = new JTabbedPane();
@@ -21,9 +20,9 @@ public class WindowWithSettings extends JDialog {
         setSize(400, 400);
 
         tabbedPane.setPreferredSize(new Dimension(300, 300));
-        JPanel design = new FirstPanel();
+        // JPanel design = new FirstPanel();
         JPanel factorySelection = new SecondPanel();
-        tabbedPane.addTab("Дизайн", design);
+        // tabbedPane.addTab("Дизайн", design);
         tabbedPane.addTab("Выбор фабрики", factorySelection);
         tabbedPane.setBackground(Color.lightGray);
         tabbedPane.setForeground(Color.white);
@@ -36,41 +35,6 @@ public class WindowWithSettings extends JDialog {
 
     public static TabulatedFunctionFactory getFactory() {
         return factory;
-    }
-
-    private static class ColorOption extends JRadioButton {
-        private final Color color;
-        public ColorOption(String colorName, Color color) {
-            super((colorName));
-            this.color = color;
-        }
-        public Color getColor() {
-            return color;
-        }
-    }
-
-    class FirstPanel extends JPanel{
-        ArrayList<ColorOption> colorOptions = new ArrayList<>();
-
-        public FirstPanel() {
-            colorOptions.add(new ColorOption("Черный", Color.BLACK));
-            colorOptions.add(new ColorOption("Белый", Color.WHITE));
-            colorOptions.add(new ColorOption("Светло-серый", Color.LIGHT_GRAY));
-            colorOptions.add(new ColorOption("Серый", Color.GRAY));
-            colorOptions.add(new ColorOption("Тёмно-серый", Color.DARK_GRAY));
-            colorOptions.add(new ColorOption("Голубой", Color.CYAN));
-            ButtonGroup buttonGroup = new ButtonGroup();
-            for (ColorOption colorOption : colorOptions) {
-                buttonGroup.add(colorOption);
-                setLayout(new FlowLayout());
-                add(colorOption);
-                colorOption.addItemListener(e -> {
-                    if (e.getStateChange() == ItemEvent.SELECTED) {
-                        getContentPane().setBackground(colorOption.getColor());
-                    }
-                });
-            }
-        }
     }
 
     static class SecondPanel extends JPanel {
